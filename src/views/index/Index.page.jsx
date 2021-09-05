b8a7097b03d588039f89f4e2eb76ed550660373d
geist-2501 <blaironbute@gmail.com>
2021-09-05 17:08:47 +0100

    Minor fixes.

--- src/views/index/Index.page.jsx
+++ src/views/index/Index.page.jsx
@@ -5,16 +5,23 @@ import Section from "../../components/section/Section.comp";
 import Container from "../../components/container/Container.comp";
 import Column from "../../components/column/Column.comp";
 import Button from "../../components/button/Button.comp";
+import {GithubLink, LinkedInLink} from "../../components/mediaLink/MediaLink.comp";
 
 const IndexPage = () => {
   return (
     <Page meta={{name: "Index | BXSYS", desc: "Beep boop"}} margin={'none'}>
       <Section type="hero">
         <Container>
-          <Column to={4} className={styles.occupation}>
+          <Column>
             <h1>👋</h1>
             <h3>Hey there,</h3>
-            <h1>I'm Blair Cross.</h1>
+            <div className={styles.name_tag}>
+              <h1 className={styles.name_tag_title}>I'm Blair Cross. </h1>
+              <GithubLink size='large' />
+              <LinkedInLink size='large' />
+            </div>
+          </Column>
+          <Column to={4} className={styles.occupation}>
             <h2>A Scotland based student and software engineer.</h2>
           </Column>
           <Column className={styles.interests}>
