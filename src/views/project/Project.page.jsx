0fd044223ad5a693bb7661df9c32474a7075162d
geist-2501 <blaironbute@gmail.com>
2021-09-10 11:36:18 +0100

    Added basic project list view.

--- src/views/project/Project.page.jsx
+++ src/views/project/Project.page.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import styles from './Project.module.scss';
 import Page from "../../components/page/Page.comp";
+import Section from '../../components/section/Section.comp';
+import Column from '../../components/column/Column.comp';
+import Container from '../../components/container/Container.comp';
+import Button from '../../components/button/Button.comp';
+import Bullet from '../../components/bullet/Bullet.comp';
 
-const ProjectPage = () => {
+const ProjectPage = ({ projects }) => {
   return (
     <Page>
-      <h1>Projects</h1>
+      <Section>
+        <Container>
+          <Column>
+            <h1>My Projects.</h1>
+            {projects.map((project, i) => (
+              <div key={i} className={styles.project_wrapper}>
+                <div className={styles.project_grid_wrapper}>
+                  <Bullet className={styles.bullet} />
+                  <h3 className={styles.title}>{project.title}</h3>
+                  <div className={styles.content}>
+                    <i >{project.desc}</i>
+                    <Button href={`/project/${project.name}`} className={styles.button}>
+                      Read more
+                    </Button>                  
+                  </div>
+                </div>
+                <div className={styles.image} style={{"--bg-image": "url(/projectimage.png)"}} />
+              </div>
+            ))}
+          </Column>
+        </Container>
+      </Section>
     </Page>
   );
 };
